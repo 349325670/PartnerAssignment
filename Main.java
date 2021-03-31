2c0c6d7d8c36c713e47349bb5c9fd496c1fa81d8
349325670 <81170903+349325670@users.noreply.github.com>
2021-03-30 22:34:51 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Good");
-    int x = 0;
-    if(x >= 3){
-      System.out.println("Lol");
-    }
+    
+    
  }
 }
