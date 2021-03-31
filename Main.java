95d071cf6145d467f220d44cbf982c7659cf4a46
Abdullah Ahmed <349325670@gapps.yrdsb.ca>
2021-03-31 02:20:15 +0000

    Deleted Lines

--- Main.java
+++ Main.java
@@ -1,9 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    System.out.println("Hi");
-    System.out.println("Hey");
-    System.out.println("HEY");
-    System.out.println("Heyy");
+    System.out.println("Good");
   }
 }
